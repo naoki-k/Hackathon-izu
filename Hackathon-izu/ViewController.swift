ac7aaeae4a4ccc1c5d3f9b484e499d1f9bba1f54
naoki-k <naoki.k.0820.i@gmail.com>
2021-05-26 00:57:23 +0900

    init firebase

--- Hackathon-izu/ViewController.swift
+++ Hackathon-izu/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var map: MKMapView!
+    private lazy var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
     }
-
-
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+            break
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            map.region = region
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Hackathon-izu' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Hackathon-izu
+  pod 'Firebase/Crashlytics'
+  pod 'Firebase/Analytics'
+
+  target 'Hackathon-izuTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Hackathon-izuUITests' do
+    # Pods for testing
+  end
+
+end
