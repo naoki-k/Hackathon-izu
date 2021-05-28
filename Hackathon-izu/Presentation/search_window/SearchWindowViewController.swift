6e33cec3639908f8a868aed4b9d3d0d2326c502b
Yushi.Watanabe <wsmjgw0760@yahoo.co.jp>
2021-05-29 02:22:54 +0900

    mod:　富戸温泉のピンを立てるところまで実装

--- Hackathon-izu/Presentation/search_window/SearchWindowViewController.swift
+++ Hackathon-izu/Presentation/search_window/SearchWindowViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 import WebKit
 import FloatingPanel
 
+protocol SearchWindowViewProtocol {
+    func addMarker(spot: Spot)
+}
+
 final class SearchWindowViewController: UIViewController, FloatingPanelLayout {
     let position: FloatingPanelPosition = .bottom
     let initialState: FloatingPanelState = .tip
+    var delegate: SearchWindowViewProtocol?
     var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
         return [
             .full: FloatingPanelLayoutAnchor(fractionalInset: 0.9, edge: .bottom, referenceGuide: .safeArea),
@@ -25,6 +30,9 @@ final class SearchWindowViewController: UIViewController, FloatingPanelLayout {
     @IBOutlet private weak var webView: WKWebView!
     
     @IBAction func run() {
-        googleMapScraper.scrape(keyword: "伊豆 観光", webView: webView)
+        let (result, error) = googleMapScraper.scrape(keyword: "伊豆 観光", webView: webView)
+        result.forEach { spot in
+            self.delegate?.addMarker(spot: spot)
+        }
     }
 }
--- Hackathon-izu/Service/Scraping/GoogleMapScraper.swift
+++ Hackathon-izu/Service/Scraping/GoogleMapScraper.swift
@@ -13,14 +13,15 @@ class GoogleMapScraper {
     let url = URL(string: "https://www.google.co.jp/maps")!
     
     func scrape(keyword: String, webView: WKWebView? = nil) -> ([Spot], Error?) {
-        let result: [Spot] = []
-        let error: Error? = nil
+        var result: [Spot] = []
+        var error: Error? = nil
 
         let browser = Erik(webView: webView)
         browser.layoutEngine.browse(url: url) { (document, error) in
             
         }
 
+        result.append(Spot(name: "富戸温泉", latitude: 34.9061188, longitude: 139.1322629))
         return (result, error)
     }
 }
